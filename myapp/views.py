5cb166857725031c50558c3a71470166234e76d6
daddydemir <mehmetcakmaktasi47@gmail.com>
2021-11-04 23:48:23 +0300

    debug fixed

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,7 @@ from .models import Userx
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from .forms import AddUserForm
 from django.shortcuts import redirect
 
 # Create your views here.
